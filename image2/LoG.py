394c61ca869dcaacfe2e42b356cfc65a68255e9c
Alex <alex@Alex-2.local>
2023-12-10 19:15:03 +0800

    first commit

---
+++ image2/LoG.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+pic_file = '..\HW4_test_image\image2.jpg'
+
+img = cv2.imread(pic_file, 0)
+row,col= img.shape
+
+output = (img/255).copy()
+
+log_out = cv2.copyMakeBorder(img, 2, 2, 2, 2,cv2.BORDER_CONSTANT, value=[255, 255, 255])
+
+c = [  0.0, 0.0,-1.0 , 0.0, 0.0,
+        0.0,-1.0,-2.0 ,-1.0, 0.0,
+        -1.0,-2.0, 16.0,-2.0,-1.0,
+        0.0,-1.0,-2.0 ,-1.0, 0.0,
+        0.0, 0.0,-1.0 , 0.0, 0.0]
+row =row+4
+col =col+4
+for i in range(2,row-3):
+    for j in range(2,col-3):
+        output[i-2][j-2] =c[0]  * log_out[i - 2][j - 2] + c[1]  * log_out[i - 2][j - 1] + c[2]  * log_out[i - 2][j] +c[3]  * log_out[i - 2][j + 1] + c[4]  * log_out[i - 2][j + 2] + \
+                          c[5]  * log_out[i - 1][j - 2] + c[6]  * log_out[i - 1][j - 1] + c[7]  * log_out[i - 1][j] +c[8]  * log_out[i - 1][j + 1] + c[9]  * log_out[i - 1][j + 2] + \
+                          c[10] * log_out[i    ][j - 2] + c[11] * log_out[i    ][j - 1] + c[12] * log_out[i    ][j] +c[13] * log_out[i    ][j + 1] + c[14] * log_out[i    ][j + 2] + \
+                          c[15] * log_out[i + 1][j - 2] + c[16] * log_out[i + 1][j - 1] + c[17] * log_out[i + 1][j] +c[18] * log_out[i + 1][j + 1] + c[19] * log_out[i + 1][j + 2] + \
+                          c[20] * log_out[i + 2][j - 2] + c[21] * log_out[i + 2][j - 1] + c[22] * log_out[i + 2][j] +c[23] * log_out[i + 2][j + 1] + c[24] * log_out[i + 2][j + 2]
+
+numpy_horizontal = np.hstack((img/255, output/255))
+cv2.imshow('Original v.s LoG',numpy_horizontal)
+cv2.waitKey(0)
+#cv2.imwrite('./image2/LoG.jpg',output)
---
+++ image2/sobel.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+pic_file = '..\HW4_test_image\image2.jpg'
+
+img = cv2.imread(pic_file, 0)
+row,col= img.shape
+
+
+c=[(-1,-2,-1,0,0,0,1,2,1), (-1,0,1,-2,0,2,-1,0,1)]
+
+so = (img/255).copy()
+
+for i in range(0,row-1):
+    for j in range(0,col-1):
+            type = 0
+            x =   c[type][0] * img[i - 1][j - 1]  + c[type][1] * img[i - 1][j]   + c[type][2] * img[i - 1][j + 1]+\
+                  c[type][3] * img[i    ][j - 1]  + c[type][4] * img[i    ][j]   + c[type][5] * img[i    ][j + 1]+\
+                  c[type][6] * img[i + 1][j - 1]  + c[type][7] * img[i + 1][j]   + c[type][8] * img[i + 1][j + 1]
+            type = 1
+            y =   c[type][0] * img[i - 1][j - 1]  + c[type][1] * img[i - 1][j]   + c[type][2] * img[i - 1][j + 1]+\
+                  c[type][3] * img[i    ][j - 1]  + c[type][4] * img[i    ][j]   + c[type][5] * img[i    ][j + 1]+\
+                  c[type][6] * img[i + 1][j - 1]  + c[type][7] * img[i + 1][j]   + c[type][8] * img[i + 1][j + 1]
+            so[i][j] = (x**2+y**2)**0.5
+
+numpy_horizontal = np.hstack((img/255, so/255))
+cv2.imshow('Original v.s sobel',numpy_horizontal)
+cv2.waitKey(0)
+#cv2.imwrite('./image2/sobel.jpg',so)
